cfd1a617ae7a1f1f818834543530b05914bd278b
Luana Soares <lusousa.soares11@gmail.com>
2025-10-22 14:58:34 +0200

    Update gitignore

--- 03.1.OPT-denovo/opt-denovo-phylo/opt-denovo-phylo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    for n in 9 10;
-    do
-        out=denovo-phylo.M${n}.n${n};
-        echo $out;
-        mkdir -p $out;
-        denovo_map.pl --samples ../../02_filtered-adaptor-rmv-q30-l60-L60/  --popmap ../popmap-phylo29.txt -o ./$out -M $n -n $n -T 20 --min-samples-per-pop 0.8;
-    done
